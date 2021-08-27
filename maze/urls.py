6947f043bb27c430450413bbdbe035e3a5161597
mingyeong <mingang0830@gmail.com>
2021-08-27 11:49:26 +0900

    Maze
    쿼리스트링 사용해서 path 하나로 줄이기

--- maze/urls.py
+++ maze/urls.py
@@ -3,10 +3,5 @@ import maze.views
 
 
 urlpatterns = [
-    path('', maze.views.index, name='index'),
-    path("gameover/", maze.views.gameover, name='gameover'),
-    path("2/", maze.views.two, name='two'),
-    path("6/", maze.views.six, name='six'),
-    path("7/", maze.views.seven, name='seven'),
-    path("end/", maze.views.end, name='end'),
+    path('', maze.views.index),
 ]
--- maze/views.py
+++ maze/views.py
@@ -3,24 +3,20 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'maze/index.html')
+    game = [
+        {'floor': 1, 'door': [1, 2, 3], 'success': 3},
+        {'floor': 2, 'door': [4, 5, 6], 'success': 5},
+        {'floor': 3, 'door': [7, 8, 9], 'success': 7}
+    ]
 
+    floor = int(request.GET.get('floor', 0))
+    door = int(request.GET.get('door', 0))
 
-def gameover(request):
-    return render(request, 'maze/gameover.html')
-
-
-def end(request):
-    return render(request, "maze/end.html")
-
-
-def two(request):
-    return render(request, "maze/two.html")
-
-
-def six(request):
-    return render(request, "maze/six.html")
-
-
-def seven(request):
-    return render(request, "seven.html")
+    if floor == 0 and door == 0:  # 시작
+        return render(request, 'maze/index.html', {'game': game[0]})
+    elif door == 7:  # 탈출 성공
+        return render(request, 'maze/end.html')
+    elif door == int(game[floor - 1]['success']):  # 옳은 선택지
+        return render(request, 'maze/index.html', {'game': game[floor]})
+    else:  # 틀린 선택지
+        return render(request, 'maze/gameover.html')
