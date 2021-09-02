2b7ba95283b849bd9acc840d3c72f062bd4cfe3d
mingyeong <mingang0830@gmail.com>
2021-09-02 11:40:46 +0900

    Maze
    데이타베이스 연결해서 회원가입 하면 db에 user 저장 할 수 있음.
    중복회원 가입 불가 설정 완료.
    정보 기입란이 공백이면 error메시지 띄우고 signup으로 render.

--- maze/models.py
+++ maze/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)
+    register_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = '유저'
+        verbose_name_plural = '유저'
